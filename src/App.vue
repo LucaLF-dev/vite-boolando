431f81acf13841e3a37bd30ba6f2b678af69aed2
LucaLF-dev <laficaraluca@gmail.com>
2023-11-06 16:15:44 +0100

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
-<script setup>
+<script >
+// 1) importazione del componente
+
+export default {
+  components: {
+     //2) registrazione del componente -- es: //PageHeader: PageHeader,
+    
+  },
+  data() {
+    return {
+      message: 'Hello world!'
+    }
+  }
+}
 
 </script>
 
 <template>
-  
+   <!-- 3) utilizzo del componente -->
 </template>
 
 <style >

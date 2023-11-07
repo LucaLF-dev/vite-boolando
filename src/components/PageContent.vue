a9b1b5ad1ab86a08376ec8cd54544f621a280e11
LucaLF-dev <laficaraluca@gmail.com>
2023-11-07 16:01:13 +0100

    Add - CardPAgeContent to PageContent

--- src/components/PageContent.vue
+++ src/components/PageContent.vue
@@ -1,10 +1,16 @@
 <script>
+import CardPageContent from './CardPageContent.vue';
+
+
 export default {
-	data() {
-		return {
-			
-		};
-	},
+    components: {
+        CardPageContent,
+    },
+
+    data() {
+        return {};
+    },
+   
 };
 </script>
 
@@ -13,25 +19,7 @@ export default {
         <div class="container">
             <div class="row">
                 <div v-for="n in 6" :key="n"  class="col-4">
-                        <div class="card">
-                            <figure class="card-image">
-                                <img class="first-image" src="img/1.webp" alt="">
-                                <img class="second-image" src="img/1b.webp" alt="">
-                                <span class="heart-icon"> &hearts; </span>
-                                <div class="banner-image">
-                                <span class="banner discount-icon">-50%</span>
-                                <span class="banner mission-icon">Sostenibilità</span>
-                            </div>
-                            </figure>
-                         
-                            <div class="card-description">
-                                <ul>
-                                    <li class="card-brand-description">Levi's</li>
-                                    <li class="card-name-description">relaxed fit tee unisex</li>
-                                    <li class="card-price-description"> 14,99 &euro; <span class="strike"> 29,99 &euro;</span></li>
-                                </ul>
-                            </div> 
-                        </div> 
+                       <CardPageContent/>
                 </div>
             </div>
         </div>
@@ -50,101 +38,6 @@ export default {
     padding: 10px;
 }
  
-}
-
-.card {
-    &:hover {
-        .second-image {
-    opacity: 1;
-    }
-} 
-}
-.card-image {
-    position: relative;
-}
-
-.card-marca-description {
-    font-size: 10px;
-    font-weight: 100;
-}
-
-.card-name-description {
-    font-size: 18px;
-    font-weight: 600;
-    text-transform: uppercase;
-}
-
-.card-price-description {
-    color: red;
-}
-
-span.strike {
-    text-decoration: line-through;
-    color: #1A1A1A;
-}
-
-.heart-icon {
-    background-color: white;
-    width: 40px;
-    height: 40px;
-    position: absolute;
-    top: 20px ;
-    right: 0;
-    display: block;
-    padding: 10px;
-    text-align: center;
-    z-index: 1;
-}
-
-
-
-
-.banner-image  {
-    position: absolute;
-    width: 100%;
-    bottom: 20px;
-    left: 0;
-    .banner { 
-    color: white;
-    font-size: 13px;
-    font-weight: 500;
-    height: 20px;
-    padding: 3px;
-}
-
-.discount-icon {
-    background-color: red;
-    width: 5px;
-
-}
-
-.mission-icon {
-    background-color: green;
-    width: 20px;
-    position: relative;
-    left: 8px;
-} 
-}
-
-.discount-icon {
-    background-color: red;
-    width: 5px;
-
-}
-
-.mission-icon {
-    background-color: green;
-    position: relative;
-    left: 10px;
-} 
-
-
-.second-image {
-    position: absolute;
-    top: 0;
-    left: 0;
-    opacity: 0;
-    z-index: 0;
 }
 
 
